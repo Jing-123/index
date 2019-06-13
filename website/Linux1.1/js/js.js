665576c785a059fc21ab8781f33ed137733bd883
Jing-123 <577275064@qq.com>
2019-06-13 13:36:32 +0800

    first

---
+++ website/Linux1.1/js/js.js
@@ -0,0 +1,71 @@
+function onmouse_1() {
+    document.getElementById("td_1").style.fontStyle="italic";
+    document.getElementById("td_1").style.color="blue";
+    document.getElementById("tb1").style.background="#d3d3d3";
+    // alert("触动了js函数！");
+}
+function outmouse_1() {
+    document.getElementById("td_1").style.fontStyle="normal";
+    document.getElementById("td_1").style.color='black';
+    document.getElementById("tb1").style.background="linear-gradient(to right, rgba(190,190,190,1), rgba(180,180,180,1))";
+}
+/////////////////////////////////////
+function onmouse_2() {
+    document.getElementById("td_2").style.fontStyle="italic";
+    document.getElementById("td_2").style.color="blue";
+    document.getElementById("tb1").style.background="#d3d3d3";
+    // alert("触动了js函数！");
+}
+function outmouse_2() {
+    document.getElementById("td_2").style.fontStyle="normal";
+    document.getElementById("td_2").style.color='black';
+    document.getElementById("tb1").style.background="linear-gradient(to right, rgba(190,190,190,1), rgba(180,180,180,1))";
+}
+/////////////////////////////////////
+function onmouse_3() {
+    document.getElementById("td_3").style.fontStyle="italic";
+    document.getElementById("td_3").style.color="blue";
+    document.getElementById("tb1").style.background="#d3d3d3";
+    // alert("触动了js函数！");
+}
+function outmouse_3() {
+    document.getElementById("td_3").style.fontStyle="normal";
+    document.getElementById("td_3").style.color='black';
+    document.getElementById("tb1").style.background="linear-gradient(to right, rgba(190,190,190,1), rgba(180,180,180,1))";
+}
+/////////////////////////////////////
+function onmouse_4() {
+    document.getElementById("td_4").style.fontStyle="italic";
+    document.getElementById("td_4").style.color="blue";
+    document.getElementById("tb1").style.background="#d3d3d3";
+    // alert("触动了js函数！");
+}
+function outmouse_4() {
+    document.getElementById("td_4").style.fontStyle="normal";
+    document.getElementById("td_4").style.color='black';
+    document.getElementById("tb1").style.background="linear-gradient(to right, rgba(190,190,190,1), rgba(180,180,180,1))";
+}
+/////////////////////////////////////
+function onmouse_5() {
+    document.getElementById("td_5").style.fontStyle="italic";
+    document.getElementById("td_5").style.color="blue";
+    document.getElementById("tb1").style.background="#d3d3d3";
+    // alert("触动了js函数！");
+}
+function outmouse_5() {
+    document.getElementById("td_5").style.fontStyle="normal";
+    document.getElementById("td_5").style.color='black';
+    document.getElementById("tb1").style.background="linear-gradient(to right, rgba(190,190,190,1), rgba(180,180,180,1))";
+}
+/////////////////////////////////////
+function onmouse_6() {
+    document.getElementById("td_6").style.fontStyle="italic";
+    document.getElementById("td_6").style.color="blue";
+    document.getElementById("tb1").style.background="#d3d3d3";
+    // alert("触动了js函数！");
+}
+function outmouse_6() {
+    document.getElementById("td_6").style.fontStyle="normal";
+    document.getElementById("td_6").style.color='black';
+    document.getElementById("tb1").style.background="linear-gradient(to right, rgba(190,190,190,1), rgba(180,180,180,1))";
+}
